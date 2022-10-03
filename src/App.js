4ff84f4dc5c2da0a8c1f9242a227f512ecb78b67
Patrick Sheil <mrpasheil@gmail.com>
2022-10-02 21:14:00 -0300

    Firebase User Authentication

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import { auth, db } from "./firebase";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Button, Input } from "@mui/material";
 
 function App() {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([
     // {
     //   username: "Patrick",
@@ -29,18 +51,141 @@ function App() {
   // UseEffect Runs a piece of code based on a specific condition
 
   useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out...
+        setUser(null);
+      }
+    })
+
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    }
+  }, [user, username]);
+
+  useEffect(() => {
     // this is where the code runs
     db.collection("posts").onSnapshot((snapshot) => {
       // every time a new post is added, this code fires.
-      setPosts(snapshot.docs.map((doc) => ({
-        id: doc.id,
-        post: doc.data()
-      })));
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
     });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+    
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+  
+  
+  const signIn = (event) => {
+    event.preventDefault();
+    
+    auth.signInWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  }
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        // aria-labelledby="modal-modal-title"
+        // aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form className="app__signup">
+          <center>
+          <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          // src= {logo}
+          alt=""
+          />
+          </center>
+
+          <Input 
+          placeholder="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input 
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input 
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+
+          {/* <h2>I am a modal</h2> */}
+          {/* <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography> */}
+        </Box>
+      </Modal>
+
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+      >
+        <Box sx={style}>
+          <form className="app__signup">
+          <center>
+          <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+          />
+          </center>
+
+          <Input 
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input 
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" onClick={signIn}>Sign In</Button>
+          </form>
+        </Box>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -49,10 +194,18 @@ function App() {
           alt=""
         />
       </div>
+          {user ? (
+            <Button onClick={() => auth.signOut()}>Logout</Button>
+          ) : (
+            <div className="app__loginContainer">
+              <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+              <Button onClick={() => setOpen(true)}>Sign Up</Button>
+            </div>
+          )}
 
       <h1>Instagram Clone!</h1>
 
-      {posts.map(({id, post}) => (
+      {posts.map(({ id, post }) => (
         <Post
           key={id}
           username={post.username}
